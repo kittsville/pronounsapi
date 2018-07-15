90baec2096735e5bee98d585b5c20082347ea80b
WheresAlice <WheresAlice@users.noreply.github.com>
2018-07-15 18:13:09 +0100

    handle any domain name

--- lib/dns.go
+++ lib/dns.go
@@ -8,12 +8,17 @@ import (
 	"strings"
 )
 
+func parseHostname(fqdn string) string {
+	return strings.Split(fqdn, ".")[0]
+}
+
 func parseQuery(m *dns.Msg) {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeTXT:
 			log.Printf("Query for %s\n", q.Name)
-			txt := database.GetUserData(strings.TrimSuffix(q.Name, ".")).Pronouns
+			username := parseHostname(q.Name)
+			txt := database.GetUserData(username).Pronouns
 			rr, err := dns.NewRR(fmt.Sprintf("%s TXT %s", q.Name, txt))
 			if err != nil {
 				log.Print(err)
